1e2383826dc848c97cb66961feb034da7cc5ae5a
Federico Ponte <fedep3@gmail.com>
2016-04-04 20:19:12 -0700

    Added save button and documents

--- app/src/main/java/edu/cmu/jsphdev/picky/fragment/AccountFragment.java
+++ app/src/main/java/edu/cmu/jsphdev/picky/fragment/AccountFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TabHost;
 
 import edu.cmu.jsphdev.picky.R;
 
@@ -16,12 +18,22 @@ public class AccountFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        Button uploadButton = (Button) view.findViewById(R.id.saveButton);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TabHost tabHost = (TabHost) getActivity().findViewById(R.id.homeTabHost);
+
+                tabHost.setCurrentTab(0);
+            }
+        });
+        return view;
     }
 
 }
