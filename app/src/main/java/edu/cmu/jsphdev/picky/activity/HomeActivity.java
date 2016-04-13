21520337afc637f3e2330da369cecf7fc05e48a0
Gaurav Walia <email.gaurav.walia@gmail.com>
2016-04-13 15:19:15 -0700

    Partial upload successful

--- app/src/main/java/edu/cmu/jsphdev/picky/activity/HomeActivity.java
+++ app/src/main/java/edu/cmu/jsphdev/picky/activity/HomeActivity.java
@@ -1,9 +1,24 @@
 package edu.cmu.jsphdev.picky.activity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 import edu.cmu.jsphdev.picky.R;
 
@@ -36,6 +51,70 @@ public class HomeActivity extends AppCompatActivity {
         spec.setContent(contentId);
         spec.setIndicator("", ContextCompat.getDrawable(this, icon));
         tabHost.addTab(spec);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        int buttonId = Integer.parseInt(((TextView) findViewById(R.id.selectedButtonForCapture)).getText().toString());
+        ImageView viewImage = (ImageView) findViewById(buttonId);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("temp.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    viewImage.setImageBitmap(bitmap);
+
+                    String path = android.os.Environment
+                            .getExternalStorageDirectory()
+                            + File.separator
+                            + "Phoenix" + File.separator + "default";
+                    f.delete();
+                    OutputStream outFile = null;
+                    File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    try {
+                        outFile = new FileOutputStream(file);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
+                        outFile.flush();
+                        outFile.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                viewImage.setImageBitmap(thumbnail);
+            }
+        }
     }
 
 }
--- app/src/main/java/edu/cmu/jsphdev/picky/fragment/UploadFragment.java
+++ app/src/main/java/edu/cmu/jsphdev/picky/fragment/UploadFragment.java
@@ -1,36 +1,77 @@
 package edu.cmu.jsphdev.picky.fragment;
 
 
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TabHost;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
 
 import edu.cmu.jsphdev.picky.R;
 
 public class UploadFragment extends Fragment {
 
+    View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_upload, container, false);
+        view = inflater.inflate(R.layout.fragment_upload, container, false);
 
-        Button choice1 = (Button) view.findViewById(R.id.choice1);
+        selectImageOnClick(R.id.choice1);
+        selectImageOnClick(R.id.choice2);
 
-        choice1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, 100);
-            }
-        });
 
         return view;
     }
+
+    private void selectImageOnClick(final int imageId) {
+        ImageView image = (ImageView) view.findViewById(imageId);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView) view.findViewById(R.id.selectedButtonForCapture)).setText(String.valueOf(imageId));
+                selectImage(imageId);
+            }
+        });
+    }
+
+    private void selectImage(final int imageId) {
+
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+
+        Builder builder = new Builder(getActivity());
+        builder.setTitle("Add a picky!");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    getActivity().startActivityForResult(intent, 1);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    getActivity().startActivityForResult(intent, 2);
+                } else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        builder.show();
+    }
+
 
 }
