5e2b329e002d6fc27c202cb8fe88f83ef90593a9
Federico Ponte <fedep3@gmail.com>
2016-04-14 14:41:36 -0700

    Votes % my pickies

--- app/src/main/java/edu/cmu/jsphdev/picky/fragment/ProfileFragment.java
+++ app/src/main/java/edu/cmu/jsphdev/picky/fragment/ProfileFragment.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import edu.cmu.jsphdev.picky.R;
 import edu.cmu.jsphdev.picky.entities.Photo;
@@ -29,6 +30,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        //TODO: Get list from server
         ArrayList<Picky> pickies = new ArrayList<Picky>();
 
         pickies.add(getPicky());
@@ -64,15 +66,21 @@ public class ProfileFragment extends Fragment {
             Button leftButton = (Button) convertView.findViewById(R.id.leftChoiceButton);
             Button rightButton = (Button) convertView.findViewById(R.id.rightChoiceButton);
             TextView title = (TextView) convertView.findViewById(R.id.profilePickyTitle);
+            TextView leftVotes = (TextView) convertView.findViewById(R.id.leftVotes);
+            TextView rightVotes = (TextView) convertView.findViewById(R.id.rightVotes);
 
             ImageDownloaderButtonCallback leftButtonCallback = new ImageDownloaderButtonCallback(getResources(), leftButton);
             ImageDownloaderButtonCallback rightButtonCallback = new ImageDownloaderButtonCallback(getResources(), rightButton);
             ImageDownloaderTask<Button> leftImageDownloaderTask = new ImageDownloaderTask<>(leftButtonCallback);
             ImageDownloaderTask<Button> rightImageDownloaderTask = new ImageDownloaderTask<>(rightButtonCallback);
 
+            int total = picky.getLeftVotes() + picky.getRightVotes();
+
             leftImageDownloaderTask.execute(picky.getLeftPhoto().getUrl());
             rightImageDownloaderTask.execute(picky.getRightPhoto().getUrl());
             title.setText(picky.getTitle());
+            leftVotes.setText(String.format(Locale.US, "%.2f %% (%d)", ((double) picky.getLeftVotes() * 100.0)/total, picky.getLeftVotes()));
+            rightVotes.setText(String.format(Locale.US, "%.2f %% (%d)", ((double) picky.getRightVotes() * 100.0)/total, picky.getRightVotes()));
             return convertView;
         }
     }
@@ -88,6 +96,8 @@ public class ProfileFragment extends Fragment {
         rightPhoto.setUrl("http://www.vegetarian-shoes.co.uk/Portals/42/product/images/prd06da61c8-f8a9-402a-8590-fbec98bfbf1a.jpg");
         picky.setLeftPhoto(leftPhoto);
         picky.setRightPhoto(rightPhoto);
+        picky.setLeftVotes(20);
+        picky.setRightVotes(80);
         return picky;
     }
 
