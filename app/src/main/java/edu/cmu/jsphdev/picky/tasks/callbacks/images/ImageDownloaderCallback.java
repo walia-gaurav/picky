2b7c1685115be203f3978fea9dfc2288e0b5c45f
Gaurav Walia <email.gaurav.walia@gmail.com>
2016-04-29 16:19:53 -0700

    Combining ImageCallbacks to work for two images, and not one.

--- app/src/main/java/edu/cmu/jsphdev/picky/tasks/callbacks/images/ImageDownloaderCallback.java
+++ app/src/main/java/edu/cmu/jsphdev/picky/tasks/callbacks/images/ImageDownloaderCallback.java
@@ -3,16 +3,20 @@ package edu.cmu.jsphdev.picky.tasks.callbacks.images;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 
+import java.util.List;
+
 public abstract class ImageDownloaderCallback<T> {
 
-    protected final T element;
+    protected final T leftElement;
+    protected final T rightElement;
     protected final Resources resources;
 
-    public ImageDownloaderCallback(Resources resources, T element) {
-        this.element = element;
+    public ImageDownloaderCallback(Resources resources, T leftElement, T rightElement) {
+        this.leftElement = leftElement;
+        this.rightElement = rightElement;
         this.resources = resources;
     }
 
-    public abstract void process(Bitmap bitmap);
+    public abstract void process(List<Bitmap> bitmap);
 
 }
